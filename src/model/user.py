cf3c330355095d1bde972ef23c25756c12e7229d
Darrell-Ha <hatrongdat219@gmail.com>
2023-02-26 18:05:32 +0700

    Update login_processing to services

--- src/model/user.py
+++ src/model/user.py
@@ -13,3 +13,22 @@ class Account(BaseModel):
     birthday: date
     secret_token: str
 
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+class SignUpRequest(BaseModel):
+    user_type: str
+    username: str
+    password: str
+    first_name: str
+    last_name: str
+    phone_number: str
+    identity_number: str
+    birthday: date
+
+__all__ = [
+    "Account",
+    "SignUpRequest",
+    "LoginRequest"
+]
